f7967a19679bc328b87380d5f9b5ba2220f1a1a4
Nabetse28 <esteban_hv.09@hotmail.com>
2019-08-10 21:42:38 -0600

    Adding files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Taller2-SE
+
+## Static Library
+
+```bash
+
+make static
+./calculadora
+```
+
+## Dinamic Library
+```bash
+
+make dinamic
+./calculadora
+```
+
---
+++ src/biblioteca.h
@@ -0,0 +1,11 @@
+#ifndef _BIBLIOTECA_H
+#define _BIBLIOTECA_H
+
+
+float sum(float a, float b);
+float sub(float a, float b);
+float mul(float a, float b);
+float div(float a, float b);
+float sqr(float a);
+
+#endif
